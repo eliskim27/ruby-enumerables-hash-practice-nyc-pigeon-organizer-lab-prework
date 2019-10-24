1e4b1927a1c22d826044233059c310e876c5a16a
Eli Kim <eliskim27@gmail.com>
2019-10-24 02:38:31 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,29 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  pigeon_list = {}
+  pigeon_data[:lives].keys.each do |location|              #iterate over lives hash
+    pigeon_data[:lives[location]].each do |name|      #iterate over location
+
+      pigeon_list["#{pigeon_data[:lives][location][name]}"] = {}
+    end
+  end
+  
+  
+  pigeon_list
 end
+
+
+
+
+
+
+
+
+
+
+
+# arrays within hashes within hash
+
+# pigeon_data
+#   color {purple, grey, white, brown}
+#   gender {male, female}
+#   lives {subway, central park, library, city hall}
